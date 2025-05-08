407673bbed19072c7772bb79c35f31c3ba8bb371
Csquare21997 <biscuitxc@gmail.com>
2025-05-08 01:42:04 -0400

    1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // require your server and launch it here
 const server = require ('./api/server')
 
-// const PORT = 9000
+const PORT = 9000
 
-// server.listen(PORT, ()=>{
-//     console.log('listening on', PORT)
-// })
+server.listen(PORT, ()=>{
+    console.log('listening on', PORT)
+})
